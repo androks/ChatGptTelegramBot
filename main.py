067a8839a10b8f93dfe0f0f3e25269277c3105c1
b.oliinyk <bohdan.olynyk@gmail.com>
2023-04-29 12:25:52 +0100

    Groups support

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-import asyncio
 import logging
 import os
-from typing import Any
 
-import nest_asyncio
 from langchain import ConversationChain, PromptTemplate
 from langchain.callbacks import CallbackManager
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationSummaryBufferMemory, PostgresChatMessageHistory
-from langchain.schema import LLMResult
-from telegram import Update, Bot
-from telegram.constants import ChatAction
+from telegram import Update, Bot, Chat
+from telegram.constants import ChatAction, ChatType
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, ContextTypes, filters
 
@@ -34,23 +30,19 @@ class Callback(CallbackManager):
         self.chat_id = chat_id
         self.message_id = message_id
 
-    def on_llm_end(self, response: LLMResult, verbose: bool = False, **kwargs: Any) -> None:
-        asyncio.get_event_loop().run_until_complete(self.update_message(force=True))
+    # def on_llm_end(self, response: LLMResult, verbose: bool = False, **kwargs: Any) -> None:
+    #     asyncio.get_event_loop().run_until_complete(self.update_message(force=True))
 
-    def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-        if token:
-            self.whole_text += token
-            nest_asyncio.apply()
-            asyncio.get_event_loop().run_until_complete(self.update_message())
-
-    async def update_message(self, force: bool = False):
-        if force or (self.whole_text and len(self.whole_text) - len(self.last_message_text) > 100):
-            try:
-                self.last_message_text = self.whole_text
-                await self.bot.edit_message_text(self.whole_text, self.chat_id, self.message_id)
-            except BadRequest as e:
-                if not e.message.startswith("Message is not modified"):
-                    raise e
+    # def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+    #     if token:
+    #         self.whole_text += token
+    #         nest_asyncio.apply()
+    #         asyncio.get_event_loop().run_until_complete(self.update_message())
+    #
+    # async def update_message(self, force: bool = False):
+    #     if force or (self.whole_text and len(self.whole_text) - len(self.last_message_text) > 100):
+    #         self.last_message_text = self.whole_text
+    #         await update_message_safe(self.bot, self.last_message_text, self.chat_id, self.message_id)
 
 
 prompt_template = PromptTemplate(
@@ -74,19 +66,43 @@ async def clear_context(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    loading_message = await context.bot.send_message(chat_id, text="Loading...")
+    bot = context.bot
+    chat = update.effective_chat
+    chat_id = chat.id
+    if not await chat_type_allowed(bot, chat, update.message.text):
+        return
+    loading_message = await context.bot.send_message(
+        chat_id,
+        text="Loading...",
+        reply_to_message_id=update.message.message_id
+        if chat.type == ChatType.GROUP or chat.type == ChatType.SUPERGROUP else None
+    )
     await context.bot.send_chat_action(chat_id, action=ChatAction.TYPING)
     try:
-        callback = Callback(context.bot, chat_id, loading_message.message_id)
-        get_chat_gpt_answer(chat_id, update.message.text, callback)
+        callback = Callback(bot, chat_id, loading_message.message_id)
+        await get_chat_gpt_answer(bot, chat_id, update.message.text, loading_message.message_id, callback)
     except Exception as e:
         logging.error(e, exc_info=True)
         await context.bot.send_message(chat_id=chat_id, text=str(e))
 
 
-def get_chat_gpt_answer(chat_id: int, question: str, callback: Callback) -> str:
+async def chat_type_allowed(bot: Bot, chat: Chat, message_text: str) -> bool:
+    bot_name = (await bot.get_me()).name
+    return chat.type == ChatType.PRIVATE or \
+        ((chat.type == ChatType.GROUP or chat.type == ChatType.SUPERGROUP) and message_text.__contains__(bot_name))
+
+
+async def update_message_safe(bot: Bot, text: str, chat_id: int, message_id: int):
+    try:
+        await bot.edit_message_text(text, chat_id, message_id)
+    except BadRequest as e:
+        if not e.message.startswith("Message is not modified"):
+            raise e
+
+
+async def get_chat_gpt_answer(bot: Bot, chat_id: int, question: str, message_id: int, callback: Callback) -> str:
     response = get_chain_for_user_with(chat_id, callback).predict(input=question)
+    await update_message_safe(bot, response, chat_id, message_id)
     return response
 
 
