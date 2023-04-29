ba65178a2bf74de086f56af1d8ec0ad6466b138c
b.oliinyk <bohdan.olynyk@gmail.com>
2023-04-29 17:25:19 +0100

    Dev

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import logging
 import os
 
 from langchain import ConversationChain, PromptTemplate
-from langchain.callbacks import CallbackManager
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationSummaryBufferMemory, PostgresChatMessageHistory
 from telegram import Update, Bot, Chat
@@ -18,31 +17,6 @@ DATABASE_URL = os.environ['DATABASE_URL']
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
-
-class Callback(CallbackManager):
-    whole_text = ''
-    last_message_text = ''
-
-    def __init__(self, bot: Bot, chat_id: int, message_id: int):
-        super().__init__([])
-        self.bot = bot
-        self.chat_id = chat_id
-        self.message_id = message_id
-
-    # def on_llm_end(self, response: LLMResult, verbose: bool = False, **kwargs: Any) -> None:
-    #     asyncio.get_event_loop().run_until_complete(self.update_message(force=True))
-
-    # def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
-    #     if token:
-    #         self.whole_text += token
-    #         nest_asyncio.apply()
-    #         asyncio.get_event_loop().run_until_complete(self.update_message())
-    #
-    # async def update_message(self, force: bool = False):
-    #     if force or (self.whole_text and len(self.whole_text) - len(self.last_message_text) > 100):
-    #         self.last_message_text = self.whole_text
-    #         await update_message_safe(self.bot, self.last_message_text, self.chat_id, self.message_id)
 
 
 prompt_template = PromptTemplate(
@@ -71,16 +45,11 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = chat.id
     if not await chat_type_allowed(bot, chat, update.message.text):
         return
-    loading_message = await context.bot.send_message(
-        chat_id,
-        text="Loading...",
-        reply_to_message_id=update.message.message_id
-        if chat.type == ChatType.GROUP or chat.type == ChatType.SUPERGROUP else None
-    )
+    loading_message = await context.bot.send_message(chat_id, text="Loading...")
     await context.bot.send_chat_action(chat_id, action=ChatAction.TYPING)
     try:
-        callback = Callback(bot, chat_id, loading_message.message_id)
-        await get_chat_gpt_answer(bot, chat_id, update.message.text, loading_message.message_id, callback)
+        await get_chat_gpt_answer(bot, chat_id, update.message.text, loading_message.message_id)
+        await bot.delete_message(chat_id, loading_message.message_id)
     except Exception as e:
         logging.error(e, exc_info=True)
         await context.bot.send_message(chat_id=chat_id, text=str(e))
@@ -89,9 +58,7 @@ async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def chat_type_allowed(bot: Bot, chat: Chat, message_text: str) -> bool:
     bot_name = (await bot.get_me()).name
     logging.info(message_text)
-    logging.info(bot_name)
     logging.info(chat.type)
-    logging.info(str(message_text.__contains__(bot_name)))
     return chat.type == ChatType.PRIVATE or \
         ((chat.type == ChatType.GROUP or chat.type == ChatType.SUPERGROUP) and message_text.__contains__(bot_name))
 
@@ -104,18 +71,15 @@ async def update_message_safe(bot: Bot, text: str, chat_id: int, message_id: int
             raise e
 
 
-async def get_chat_gpt_answer(bot: Bot, chat_id: int, question: str, message_id: int, callback: Callback) -> str:
-    response = get_chain_for_user_with(chat_id, callback).predict(input=question)
-    await update_message_safe(bot, response, chat_id, message_id)
+async def get_chat_gpt_answer(bot: Bot, chat_id: int, question: str, message_id: int) -> str:
+    response = get_chain_for_user_with(chat_id).predict(input=question)
+    await bot.send_message(chat_id, response)
     return response
 
 
-def get_chain_for_user_with(telegram_id: int, callback: Callback) -> ConversationChain:
+def get_chain_for_user_with(telegram_id: int) -> ConversationChain:
     open_ai = ChatOpenAI(
-        openai_api_key=OPEN_AI_KEY,
-        streaming=True,
-        verbose=True,
-        callback_manager=callback
+        openai_api_key=OPEN_AI_KEY
     )
     memory = ConversationSummaryBufferMemory(
         llm=open_ai,
